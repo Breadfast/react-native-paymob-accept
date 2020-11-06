0f917da105f024f5c1fb2414d530b1d8cb44f7cd
ghobashy <ghobashy2@gmail.com>
2020-11-06 23:40:02 +0200

    [FEAT] update android packages

--- android/src/main/java/com/breadfast/RNPaymobAcceptModule.java
+++ android/src/main/java/com/breadfast/RNPaymobAcceptModule.java
@@ -1,4 +1,4 @@
-package com.breadfast;
+package com.breadfast.reactnative;
 
 import android.content.Intent;
 import android.os.Bundle;
--- android/src/main/java/com/breadfast/RNPaymobAcceptPackage.java
+++ android/src/main/java/com/breadfast/RNPaymobAcceptPackage.java
@@ -1,4 +1,4 @@
-package com.breadfast;
+package com.breadfast.reactnative;
 
 import java.util.Arrays;
 import java.util.Collections;
